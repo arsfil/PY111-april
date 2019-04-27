9bd02d417882c4fab570db6536da79cb78a26e00
Филимонов Арсений Викторович <ARSFIL@avalon.ru>
2019-04-27 13:41:34 +0300

    to do

--- Tasks/e0_breadth_first_search.py
+++ Tasks/e0_breadth_first_search.py
@@ -1,6 +1,36 @@
 from typing import Any
 import networkx as nx
+import matplotlib as plt
+import numpy as np
 
+g = {
+	('A', 'B'),
+	('A', 'C'),
+	('A', 'D'),
+	('B', 'E'),
+	('D', 'F'),
+	('E', 'G'),
+	('F', 'G'),
+	('G', 'H')
+}
+
+start_node = 'A'
+
+g.nodes = [
+	# [A, B, C, D, E, F, G, H]
+	[0, 1, 1, 1, 0, 0, 0, 0],
+	[1, 0, 0, 0, 1, 0, 0, 0],
+	[1, 0, 0, 0, 0, 0, 0, 0],
+	[1, 0, 0, 0, 0, 1, 0, 0],
+	[0, 1, 0, 0, 0, 0, 1, 0],
+	[0, 0, 0, 1, 0, 0, 1, 0],
+	[0, 0, 0, 0, 1, 1, 0, 1],
+	[0, 0, 0, 0, 0, 0, 1, 0]
+]
+
+
+nx.draw(g, with_label=True)
+plt.show()
 
 def bfs(g: nx.Graph, start_node: Any) -> list:
 	"""
@@ -10,5 +40,6 @@ def bfs(g: nx.Graph, start_node: Any) -> list:
 	:param start_node: starting node for search
 	:return: list of nodes in the visited order
 	"""
+
 	print(g, start_node)
 	return list(g.nodes)
